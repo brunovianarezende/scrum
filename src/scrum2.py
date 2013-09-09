cc549742a7536a86f07ae1861d3c4f686df25881
Bruno Rezende <brunovianarezende@gmail.com>
2013-09-08 22:01:08 -0300

    finished first version of ply based parsed

--- src/scrum2.py
+++ src/scrum2.py
@@ -1,27 +1,54 @@
 import ply.lex as lex
+import ply.yacc as yacc
 
 class ScrumLexer(object):
     tokens = ('DAY', 'EOL', 'TIME', 'NA', 'NUMBER', 'LPARENTHESIS',
-        'RPARENTHESIS', 'DESCRIPTION', 'ACTIVITY', 'SEPARATOR',
+        'RPARENTHESIS', 'TITLE', 'ACTIVITY', 'SEPARATOR', 'NUMBERSIGN',
+        'DEFAULTPROJECT', 'PROJECTNAME', 'PROJECT',
     )
-    literals = ('#',)
     states = (
         ('taskdescription', 'exclusive'),
         ('activity', 'exclusive'),
+        ('defaultproject', 'exclusive'),
+        ('project', 'exclusive'),
     )
     
     t_DAY = r'\d\d/\d\d/\d\d\d\d'
     t_EOL = r'\n'
-    t_TIME = r'\d\d:\d\d'
+    t_NUMBERSIGN = r'\#'
+    t_TIME = r'\d?\d:\d\d'
     t_NA = r'NA'
     t_NUMBER = r'\d+'
     t_ignore  = ' \t'
-    t_taskdescription_DESCRIPTION = r'[^\)]+'
+    t_taskdescription_TITLE = r'[^\)]+'
     t_activity_ACTIVITY = r'[^\n]+'
+    t_defaultproject_PROJECTNAME = r'[^\n]+'
+    t_project_PROJECTNAME = r'[^\n]+'
+    t_project_ignore = ''
+    t_defaultproject_ignore = ''
     t_taskdescription_ignore = ''
     t_activity_ignore = ''
 
-    # Match the first (. Enter text state.
+    def t_PROJECT(self, t):
+        r'Project:'
+        t.lexer.begin('project')
+        return t
+
+    def t_project_EOL(self, t):
+        r'\n'
+        t.lexer.begin('INITIAL')
+        return t
+
+    def t_DEFAULTPROJECT(self, t):
+        r'DefaultProject:'
+        t.lexer.begin('defaultproject')
+        return t
+
+    def t_defaultproject_EOL(self, t):
+        r'\n'
+        t.lexer.begin('INITIAL')
+        return t
+
     def t_LPARENTHESIS(self, t):
         r'\('
         t.type = 'LPARENTHESIS'
@@ -55,31 +82,130 @@ class ScrumLexer(object):
         print "activity: Illegal character '%s'" % t.value[0]
         t.lexer.skip(1)
 
+    def t_defaultproject_error(self, t):
+        print "defaultproject: Illegal character '%s'" % t.value[0]
+        t.lexer.skip(1)
+
+    def t_project_error(self, t):
+        print "project: Illegal character '%s'" % t.value[0]
+        t.lexer.skip(1)
+
     def build(self,**kwargs):
         self.lexer = lex.lex(module=self, **kwargs)
 
 
-"""
-26/07/2013
-7:17 9:14
-10:00 12:06
-#4133 (Make Mariupol site and agent work together) - doing
+def convert_to_minutes(timestr):
+    """
+    >>> convert_to_minutes('9:54')
+    594
+    """
+    hour, minutes = timestr.split(':')
+    hour = int(hour.lstrip('0') or 0)
+    minutes = int(minutes.lstrip('0') or 0)
+    return hour * 60 + minutes
 
-dayinfo : DAY \n interval \n activity
-interval : TIME TIME | semiinterval
-semiinterval : TIME
-activity : issue (words) - words
-issue : NA | #NUMBER
-words : words WORD | WORD
-"""
 
-# lexer = lex.lex()
+class PlyScrumParser(object):
+    def __init__(self, default_project):
+        lexer = ScrumLexer()
+        lexer.build()
+        self.default_project = default_project
+        self._current_project = default_project
+        self.lexer = lexer.lexer
+        self.tokens = lexer.tokens
+        self.yacc = yacc.yacc(module=self)
 
-# def main():
-#     text = "26/07/2013\n"
-#     lexer.input(text)
-#     for token in lexer:
-#         print token
-# 
-# if __name__ == '__main__':
-#     main()
+    def parse(self, input_string):
+        input_string = input_string.strip() + '\n'
+        result = self.yacc.parse(input_string, lexer=self.lexer)
+        self._current_project = self.default_project
+        return result
+
+    def p_content(self, p):
+        """content : dayinfo
+                   | defaultproject"""
+        p[0] = p[1]
+
+    def p_dayinfo(self, p):
+#         'dayinfo : DAY EOL intervals EOL activities'
+        'dayinfo : DAY EOL projectsdata'
+        result = [dict(d) for d in p[3]]
+        for d in result:
+            d.update(**{'day': p[1]})
+        p[0] = result
+
+    def p_projectsdata_multiple(self, p):
+        'projectsdata : projectsdata changeprojectinfo projectdata'
+        p[0] = p[1] + [p[3]]
+
+    def p_changeprojectinfo(self, p):
+        'changeprojectinfo : PROJECT PROJECTNAME EOL'
+        self._current_project = p[2].strip()
+
+    def p_projectsdata_single(self, p):
+        'projectsdata : projectdata'
+        p[0] = [p[1]]
+
+    def p_projectdata(self, p):
+        'projectdata : intervals activities'
+        p[0] = {'work_time': p[1], 'activities': p[2],
+            'project': self._current_project}
+
+    def p_defaultproject(self, p):
+        'defaultproject : DEFAULTPROJECT PROJECTNAME EOL'
+        self.default_project = p[2].strip()
+
+    def p_intervals_multiple(self, p):
+        'intervals : intervals interval'
+        p[0] = p[1] + p[2]
+
+    def p_intervals_single(self, p):
+        'intervals : interval'
+        p[0] = p[1]
+
+    def p_interval(self, p):
+        'interval : TIME TIME EOL'
+        p[0] = convert_to_minutes(p[2]) - convert_to_minutes(p[1])
+
+    def p_activities(self, p):
+        'activities : activity'
+        p[0] = [p[1]]
+
+    def p_activities_multiple(self, p):
+        'activities : activities activity'
+        p[0] = p[1] + [p[2]]
+
+    def p_activity_single(self, p):
+        'activity : taskidentifier LPARENTHESIS TITLE RPARENTHESIS SEPARATOR ACTIVITY EOL'
+        p[0] = {
+            'description': p[6].strip(),
+            'ticket': p[1],
+            'title': p[3].strip(),
+        }
+
+    def p_taskidentifier_available(self, p):
+        'taskidentifier : NUMBERSIGN NUMBER'
+        p[0] = p[2]
+
+    def p_taskidentifier_notavailable(self, p):
+        'taskidentifier : NA'
+        p[0] = p[1]
+
+class ScrumParser(object):
+    def __init__(self, default_project):
+        self.parser = PlyScrumParser(default_project=default_project)
+
+    def parse(self, lines):
+        current_lines = []
+        for line in lines:
+            line = line.strip()
+            if line:
+                current_lines.append(line)
+            elif not current_lines:
+                pass
+            else:
+                item_str = '\n'.join(current_lines)
+                current_lines = []
+                result = self.parser.parse(item_str)
+                if result:
+                    yield result
--- src/test_scrum2.py
+++ src/test_scrum2.py
@@ -1,6 +1,6 @@
 import unittest
 
-from scrum2 import ScrumLexer
+from scrum2 import ScrumLexer, PlyScrumParser, ScrumParser
 
 class TestScrumLexer(unittest.TestCase):
     def test_tokenizer(self):
@@ -8,7 +8,9 @@ class TestScrumLexer(unittest.TestCase):
         lexer.build()
         lexer_input = """\
 01/12/2013 
-12:45 NA #12345(a nice 123 word)- Word woRd2
+12:45 2:31 NA #12345(a nice 123 word)- Word woRd2
+DefaultProject: Nice Project
+Project: new Project name
 """
         lexer.lexer.input(lexer_input)
         tokens = list(lexer.lexer)
@@ -16,14 +18,21 @@ class TestScrumLexer(unittest.TestCase):
             ('DAY', '01/12/2013'),
             ('EOL', '\n'),
             ('TIME', '12:45'),
+            ('TIME', '2:31'),
             ('NA', 'NA'),
-            ('#', '#'),
+            ('NUMBERSIGN', '#'),
             ('NUMBER', '12345'),
             ('LPARENTHESIS', '('),
-            ('DESCRIPTION', 'a nice 123 word'),
+            ('TITLE', 'a nice 123 word'),
             ('RPARENTHESIS', ')'),
             ('SEPARATOR', '-'),
             ('ACTIVITY', ' Word woRd2'),
+            ('EOL', '\n'),
+            ('DEFAULTPROJECT', 'DefaultProject:'),
+            ('PROJECTNAME', ' Nice Project'),
+            ('EOL', '\n'),
+            ('PROJECT', 'Project:'),
+            ('PROJECTNAME', ' new Project name'),
             ('EOL', '\n'),
         ]
         for i, (ttype, value) in enumerate(expected):
@@ -31,3 +40,229 @@ class TestScrumLexer(unittest.TestCase):
             self.assertEqual(tokens[i].type, ttype, msg)
             self.assertEqual(tokens[i].value, value, msg)
         self.assertEqual(len(tokens), len(expected), '%s - %s' % (expected, tokens))
+
+class TestParser(unittest.TestCase):
+    def test_parse(self):
+        parser = PlyScrumParser(default_project='Project')
+        parser_input = """\
+26/07/2013
+7:17 9:14
+10:00 12:06
+#4133 (Make Mariupol site and agent work together) - doing
+#1 (new test) - doing too
+NA (Not Available) - testing everything
+"""
+        expected = [
+            {
+            'activities': [
+                {
+                 'description': 'doing',
+                 'ticket': '4133',
+                 'title': 'Make Mariupol site and agent work together'
+                 },
+                {
+                 'description': 'doing too',
+                 'ticket': '1',
+                 'title': 'new test'
+                 },
+                {
+                 'description': 'testing everything',
+                 'ticket': 'NA',
+                 'title': 'Not Available'
+                 },
+            ],
+            'day': '26/07/2013',
+            'work_time': 243,
+            'project': 'Project',
+            }
+        ]
+        for p in parser_input.strip(), parser_input.strip() + '\n':
+            result = parser.parse(p)
+            self.assertEqual(result, expected)
+
+        parser = PlyScrumParser(default_project='Project')
+        parser_input = """\
+DefaultProject: my Nice project
+"""
+        expected = None
+        for p in parser_input.strip(), parser_input.strip() + '\n':
+            result = parser.parse(p)
+            self.assertEqual(result, expected)
+            self.assertEqual(parser.default_project, 'my Nice project')
+
+        parser = PlyScrumParser(default_project='Project')
+        parser_input = """\
+26/07/2013
+7:17 9:14
+10:00 12:06
+#4133 (Make Mariupol site and agent work together) - doing
+#1 (new test) - doing too
+NA (Not Available) - testing everything
+Project: New Project Name
+1:14 2:05
+#1234 (First) - First activity
+NA (Second) - Second activity
+#3 (Third) - Third activity
+"""
+        expected = [
+            {
+             'activities': [
+                {
+                 'description': 'doing',
+                 'ticket': '4133',
+                 'title': 'Make Mariupol site and agent work together'
+                 },
+                {
+                 'description': 'doing too',
+                 'ticket': '1',
+                 'title': 'new test'
+                 },
+                {
+                 'description': 'testing everything',
+                 'ticket': 'NA',
+                 'title': 'Not Available'
+                 },
+            ],
+            'day': '26/07/2013',
+            'work_time': 243,
+            'project': 'Project',
+            },
+            {
+             'activities': [
+                {
+                 'description': 'First activity',
+                 'ticket': '1234',
+                 'title': 'First',
+                 },
+                {
+                 'description': 'Second activity',
+                 'ticket': 'NA',
+                 'title': 'Second',
+                 },
+                {
+                 'description': 'Third activity',
+                 'ticket': '3',
+                 'title': 'Third'
+                 },
+            ],
+            'day': '26/07/2013',
+            'work_time': 51,
+            'project': 'New Project Name',
+            },
+        ]
+        for p in parser_input.strip(), parser_input.strip() + '\n':
+            result = parser.parse(p)
+            self.assertEqual(result, expected)
+
+class TestScrumParser(unittest.TestCase):
+    def test_parse(self):
+        parser_input = """\
+DefaultProject: Project
+
+26/07/2013
+7:17 9:14
+10:00 12:06
+#4133 (Make Mariupol site and agent work together) - doing
+#1 (new test) - doing too
+NA (Not Available) - testing everything
+Project: New Project Name
+1:14 2:05
+#1234 (First) - First activity
+NA (Second) - Second activity
+#3 (Third) - Third activity
+
+25/07/2013
+7:17 9:14
+10:00 12:06
+#1234 (title) - doing
+
+DefaultProject: other project
+
+24/07/2013
+7:17 9:14
+10:00 12:06
+#25 (other title) - really doing
+"""
+        parser = ScrumParser('')
+        items = parser.parse(parser_input.split('\n'))
+        expected = [
+            {
+             'activities': [
+                {
+                 'description': 'doing',
+                 'ticket': '4133',
+                 'title': 'Make Mariupol site and agent work together'
+                 },
+                {
+                 'description': 'doing too',
+                 'ticket': '1',
+                 'title': 'new test'
+                 },
+                {
+                 'description': 'testing everything',
+                 'ticket': 'NA',
+                 'title': 'Not Available'
+                 },
+            ],
+            'day': '26/07/2013',
+            'work_time': 243,
+            'project': 'Project',
+            },
+            {
+             'activities': [
+                {
+                 'description': 'First activity',
+                 'ticket': '1234',
+                 'title': 'First',
+                 },
+                {
+                 'description': 'Second activity',
+                 'ticket': 'NA',
+                 'title': 'Second',
+                 },
+                {
+                 'description': 'Third activity',
+                 'ticket': '3',
+                 'title': 'Third'
+                 },
+            ],
+            'day': '26/07/2013',
+            'work_time': 51,
+            'project': 'New Project Name',
+            },
+        ]
+        self.assertEqual(items.next(), expected)
+
+        expected = [
+            {
+             'activities': [
+                {
+                 'description': 'doing',
+                 'ticket': '1234',
+                 'title': 'title'
+                 },
+            ],
+            'day': '25/07/2013',
+            'work_time': 243,
+            'project': 'Project',
+            },
+        ]
+        self.assertEqual(items.next(), expected)
+
+        expected = [
+            {
+             'activities': [
+                {
+                 'description': 'really doing',
+                 'ticket': '25',
+                 'title': 'other title'
+                 },
+            ],
+            'day': '24/07/2013',
+            'work_time': 243,
+            'project': 'other project',
+            },
+        ]
+        self.assertEqual(items.next(), expected)
+
+        self.assertRaises(StopIteration, items.next)
