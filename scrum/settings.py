38626d0cae5facc4c14739b94fa79b53ef66e1e0
Bruno Rezende <brunovianarezende@gmail.com>
2017-11-27 16:02:43 -0200

    improve error message when local_settings can't be loaded

--- scrum/settings.py
+++ scrum/settings.py
@@ -3,5 +3,7 @@ SCRUM_FILEPATH = None # it must be setup in local_settings.py file
 
 try:
     from .local_settings import *
-except:
+    print(SCRUM_FILEPATH)
+except Exception as e:
+    print("local settings couldn't be loaded: %s" % e)
     pass
