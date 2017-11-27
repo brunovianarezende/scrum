93343a388be6b1bcc0e089b60c1546ae204581a0
Bruno Rezende <brunovianarezende@gmail.com>
2017-11-27 16:18:43 -0200

    small improvement to README

--- scrum/settings.py
+++ scrum/settings.py
@@ -3,7 +3,6 @@ SCRUM_FILEPATH = None # it must be setup in local_settings.py file
 
 try:
     from .local_settings import *
-    print(SCRUM_FILEPATH)
 except Exception as e:
     print("local settings couldn't be loaded: %s" % e)
     pass
